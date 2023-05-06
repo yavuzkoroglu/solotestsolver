b03de8a952165d0ed9a055cc165c130e87717682
yavuzkoroglu <yavuz.koroglu@boun.edu.tr>
2023-05-06 04:04:28 +0300

    on 06.05.23 @ 04:04

--- src/main.c
+++ src/main.c
@@ -9,14 +9,14 @@
 int main(void);
 
 int main(void) {
-    unsigned char sz = 0;
     Layout layout[2] = { INITIAL_LAYOUT, INITIAL_LAYOUT };
+    unsigned char sz;
     Decision decisionStack[CAP][CAP];
     unsigned char trace[CAP];
 
     /* Stack-based Solver */
-    decisions_layout(decisionStack[sz], layout[0]);
-    until (score_layout(layout[0]) == 1) {
+    for (sz = 0; score_layout(layout[0]) != 1; sz++) {
+        decisions_layout(decisionStack[sz], layout[0]);
         unless ((trace[sz] = count_decisions(decisionStack[sz]))) {
             until (trace[sz]) {
                 sz--;
@@ -24,7 +24,6 @@ int main(void) {
             }
         }
         applyDecision_layout(layout[0], decisionStack[sz][--trace[sz]]);
-        decisions_layout(decisionStack[++sz], layout[0]);
     }
 
     /* Print the solution! */
