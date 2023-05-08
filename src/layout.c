793552f8d0015263c4cfc42832f54726231f71fb
yavuzkoroglu <yavuz.koroglu@boun.edu.tr>
2023-05-09 02:21:40 +0300

    on 09.05.23 @ 02:21

--- src/layout.c
+++ src/layout.c
@@ -11,6 +11,36 @@ void print_layout(Layout const layout) {
 unsigned char populateDecisions_layout(Decision* const restrict decisions, Layout const layout) {
     unsigned char sz = 0;
 
+    /*
+    if (layout[0][2] == 'o') {
+        if (layout[0][3] == 'o' && layout[0][4] == '.')
+            decisions[sz++] = (Decision){ 0, 2, DIR_EAST };
+        if (layout[1][2] == 'o' && layout[2][2] == '.')
+            decisions[sz++] = (Decision){ 0, 2, DIR_SOUTH };
+    }
+    if (layout[0][3] == 'o' && layout[1][3] == 'o' && layout[2][3] == '.')
+        decisions[sz++] = (Decision){ 0, 3, DIR_SOUTH };
+    if (layout[0][4] == 'o') {
+        if (layout[0][3] == 'o' && layout[0][2] == '.')
+            decisions[sz++] = (Decision){ 0, 4, DIR_WEST };
+        if (layout[1][4] == 'o' && layout[2][4] == '.')
+            decisions[sz++] = (Decision){ 0, 2, DIR_SOUTH };
+    }
+    if (layout[1][2] == 'o') {
+        if (layout[1][3] == 'o' && layout[1][4] == '.')
+            decisions[sz++] = (Decision){ 0, 2, DIR_EAST };
+        if (layout[2][2] == 'o' && layout[3][2] == '.')
+            decisions[sz++] = (Decision){ 0, 2, DIR_SOUTH };
+    }
+    if (layout[0][3] == 'o' && layout[1][3] == 'o' && layout[2][3] == '.')
+        decisions[sz++] = (Decision){ 0, 3, DIR_SOUTH };
+    if (layout[0][4] == 'o') {
+        if (layout[0][3] == 'o' && layout[0][2] == '.')
+            decisions[sz++] = (Decision){ 0, 4, DIR_WEST };
+        if (layout[1][4] == 'o' && layout[2][4] == '.')
+            decisions[sz++] = (Decision){ 0, 2, DIR_SOUTH };
+    }*/
+
     for (unsigned char row = 0; row < LAYOUT_NROWS; row++) {
         for (unsigned char col = 0; col < LAYOUT_NCOLS; col++) {
             /* If no peg, no moves! */
@@ -38,44 +68,15 @@ unsigned char populateDecisions_layout(Decision* const restrict decisions, Layou
     return sz;
 }
 
+static const char rowOffsets[4][2] = { { 0, 0 }, { 1, 2 }, { 0, 0 }, { -1, -2 } };
+static const char colOffsets[4][2] = { { 1, 2 }, { 0, 0 }, { -1, -2 }, { 0, 0 } };
 void applyDecision_layout(Layout layout, Decision const decision) {
     layout[decision.row][decision.col] = '.';
-    switch (decision.dir) {
-        case DIR_EAST:
-            layout[decision.row][decision.col + 1] = '.';
-            layout[decision.row][decision.col + 2] = 'o';
-            break;
-        case DIR_SOUTH:
-            layout[decision.row + 1][decision.col] = '.';
-            layout[decision.row + 2][decision.col] = 'o';
-            break;
-        case DIR_WEST:
-            layout[decision.row][decision.col - 1] = '.';
-            layout[decision.row][decision.col - 2] = 'o';
-            break;
-        case DIR_NORTH:
-            layout[decision.row - 1][decision.col] = '.';
-            layout[decision.row - 2][decision.col] = 'o';
-    }
+    layout[decision.row + rowOffsets[decision.dir][0]][decision.col + colOffsets[decision.dir][0]] = '.';
+    layout[decision.row + rowOffsets[decision.dir][1]][decision.col + colOffsets[decision.dir][1]] = 'o';
 }
-
 void undoDecision_layout(Layout layout, Decision const decision) {
     layout[decision.row][decision.col] = 'o';
-    switch (decision.dir) {
-        case DIR_EAST:
-            layout[decision.row][decision.col + 1] = 'o';
-            layout[decision.row][decision.col + 2] = '.';
-            break;
-        case DIR_SOUTH:
-            layout[decision.row + 1][decision.col] = 'o';
-            layout[decision.row + 2][decision.col] = '.';
-            break;
-        case DIR_WEST:
-            layout[decision.row][decision.col - 1] = 'o';
-            layout[decision.row][decision.col - 2] = '.';
-            break;
-        case DIR_NORTH:
-            layout[decision.row - 1][decision.col] = 'o';
-            layout[decision.row - 2][decision.col] = '.';
-    }
+    layout[decision.row + rowOffsets[decision.dir][0]][decision.col + colOffsets[decision.dir][0]] = 'o';
+    layout[decision.row + rowOffsets[decision.dir][1]][decision.col + colOffsets[decision.dir][1]] = '.';
 }
